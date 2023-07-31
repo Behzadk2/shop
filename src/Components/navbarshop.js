d9db2ee830fd970a7c1e2fb1f43138c058c1c1da
behzad kazemi <bkazemi1994@gmail.com>
2023-07-31 18:29:18 +0200

    Add navbar completely

--- src/Components/navbarshop.js
+++ src/Components/navbarshop.js
@@ -1,7 +1,20 @@
+import {Link} from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 const Navbarshop = () => {
   return (
-    <div>
-      <h1>Navbarshop</h1>
+    <div className="navbar navbar-dark bg-dark navbar-expand-lg">
+       <div className="container">
+        <a className="navbar-brand">Behzad Shop</a>
+        <ul className="navbar-nav"> 
+          <li className="nav-item"><Link to='/' className='nav-link'>shop</Link></li>
+          <li className="nav-item">
+            <Link to='/card' className='nav-link'>
+              <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </Link>
+          </li>
+        </ul>
+       </div>
     </div>
   );
 };
